6c9aef7b83c19058dd2372b550d4279ad212c9df
ShGanesh <54804789+ShGanesh@users.noreply.github.com>
2021-05-05 17:32:30 +0100

    Update Forenscs_like1000.sh

--- Forenscs_like1000.sh
+++ Forenscs_like1000.sh
@@ -2,10 +2,9 @@
 
 for i in {999..0..-1}         # {initial..final..increment}
 do
-	tar -xvf *.tar              # Using regex... tar -x(extract)v(verbose)f(file) *(all_characters).tar.
+	tar -xvf *.tar            # Using regex... tar -x(extract)v(verbose)f(file) *(all_characters).tar.
                               # *.tar means: all files present in working directory that end with ".tar".
-	rm $(( $i+1 )).tar          # Remove file (i+1).tar
+	rm $(( $i+1 )).tar        # Remove file (i+1).tar
                               # If loop is currently in i==228, 229.tar is removed.
-	rm filler.txt               #removing filler.txt
-
+	rm filler.txt             # removing filler.txt
 done
